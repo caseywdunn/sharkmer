1f73a44607974dd35ba78af829c36dd2507384be
Casey Dunn <casey.dunn@yale.edu>
2023-05-29 12:08:20 -0400

    black

--- sharkmer_viewer/sharkmer_viewer/sharkmer_viewer.py
+++ sharkmer_viewer/sharkmer_viewer/sharkmer_viewer.py
@@ -1,4 +1,4 @@
-# Ingests a tsv file of kmer histograms, where the first column is the count 
+# Ingests a tsv file of kmer histograms, where the first column is the count
 # of kmers. The following columns are the kmer counts for each sample.
 # Outputs an animated histogram of the kmer counts for each sample.
 
@@ -9,12 +9,11 @@ import numpy as np
 import pandas as pd
 import scipy
 
+
 def main():
 
     # https://docs.python.org/3/howto/argparse.html
-    parser = argparse.ArgumentParser(
-        description="view sharkmer results"
-    )
+    parser = argparse.ArgumentParser(description="view sharkmer results")
     parser.add_argument(
         "input",
         help="input file, histogram from sharkmer",
@@ -24,20 +23,20 @@ def main():
 
     in_file_name = args.input
 
-    df = pd.read_csv(in_file_name, sep='\t', header=None)
+    df = pd.read_csv(in_file_name, sep="\t", header=None)
 
     # Truncate the data
     df = df.iloc[:100]
 
     # Based generally on https://matplotlib.org/stable/gallery/animation/simple_anim.html
     # Get the counts, then remove the column
-    x = df.iloc[:,0]
+    x = df.iloc[:, 0]
     x = np.array(x)
     df = df.drop(df.columns[0], axis=1)
 
     # Calculate the limits of the plot based on the characteristics of the peak, if there is one
     # Get the last column of the dataframe as a numpy array
-    y = df.iloc[:,-1]
+    y = df.iloc[:, -1]
     y = np.array(y)
 
     x_limit = 100
@@ -58,10 +57,10 @@ def main():
     ax.set_xlim(0, x_limit)
     ax.set_ylim(0, y_limit)
 
-    line, = ax.plot(x, pd.Series([0]*len(x)))
+    (line,) = ax.plot(x, pd.Series([0] * len(x)))
 
     def animate(i):
-        y = df.iloc[:,i]
+        y = df.iloc[:, i]
         # convert to numpy array
         y = np.array(y)
 
@@ -71,16 +70,17 @@ def main():
             tallest_peak_index = peaks[0]
             x_peak = x[tallest_peak_index]
             # Draw a vertical line at the peak
-            ax.axvline(x=x_peak, color='r', linestyle='--')
+            ax.axvline(x=x_peak, color="r", linestyle="--")
 
         line.set_ydata(y)  # update the data
-        return line,
+        return (line,)
 
-    ani = animation.FuncAnimation(fig, animate, frames=len(df.columns), 
-                              interval=25, blit=True)
+    ani = animation.FuncAnimation(
+        fig, animate, frames=len(df.columns), interval=25, blit=True
+    )
 
     # Save the animation
-    ani.save(in_file_name + ".mp4", writer='ffmpeg')
+    ani.save(in_file_name + ".mp4", writer="ffmpeg")
 
 
 if __name__ == "__main__":
