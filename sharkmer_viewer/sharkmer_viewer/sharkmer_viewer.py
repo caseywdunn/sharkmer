04e84fec79693e94edf036691d4a94d7c005d6d3
Casey Dunn <casey.dunn@yale.edu>
2023-05-29 20:55:53 -0400

    more tuning

--- sharkmer_viewer/sharkmer_viewer/sharkmer_viewer.py
+++ sharkmer_viewer/sharkmer_viewer/sharkmer_viewer.py
@@ -9,18 +9,7 @@ import numpy as np
 import pandas as pd
 import scipy
 
-def create_report(in_file_name):
-    df = pd.read_csv(in_file_name, sep="\t", header=None)
-
-    # Truncate the data
-    df = df.iloc[:100]
-
-    # Based generally on https://matplotlib.org/stable/gallery/animation/simple_anim.html
-    # Get the counts, then remove the column
-    x = df.iloc[:, 0]
-    x = np.array(x)
-    df = df.drop(df.columns[0], axis=1)
-
+def get_limits(df):
     # Calculate the limits of the plot based on the characteristics of the peak, if there is one
     # Get the last column of the dataframe as a numpy array
     y = df.iloc[:, -1]
@@ -37,33 +26,46 @@ def create_report(in_file_name):
 
         # Find the peaks
         # https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.find_peaks.html
-        peaks, properties = scipy.signal.find_peaks(y, height=0, distance=2)
+        peaks, properties = scipy.signal.find_peaks(y, height=0, distance=2, threshold=10)
 
+        x_of_tallest_peak = None
         for peak in peaks:
             # Ignore peaks that are too far to the left
             if peak > 5:
                 if y_max is None:
                     y_max = y[peak]
+                    x_of_tallest_peak = peak
                 elif y[peak] > y_max:
                     y_max = y[peak]
+                    x_of_tallest_peak = peak
+        
+        # If last column
+        if i == len(df.columns) - 1:
+            if x_of_tallest_peak is not None:
+                x_limit = x_of_tallest_peak * 3
 
     if y_max is not None:
         y_limit = y_max * 1.2
+    
+    return x_limit, y_limit
 
-    # Find the x coordinate of the peak furthest to the right in the last column, use it to scale x
-    y = df.iloc[:, -1]
-    y = np.array(y)
+def create_report(in_file_name):
+    df = pd.read_csv(in_file_name, sep="\t", header=None)
 
-    peaks, properties = scipy.signal.find_peaks(y, height=0, distance=2)
+    # Truncate the data
+    df = df.iloc[:100]
 
-    if len(peaks) > 0:
-        max_peak_index = max(peaks)
-        x_limit = x[max_peak_index] * 3
+    # Based generally on https://matplotlib.org/stable/gallery/animation/simple_anim.html
+    # Get the counts, then remove the column
+    x = df.iloc[:, 0]
+    x = np.array(x)
+    df = df.drop(df.columns[0], axis=1)
 
     # Create the plot
     fig, ax = plt.subplots()
 
     # Set the x and y limits
+    x_limit, y_limit = get_limits(df)
     ax.set_xlim(0, x_limit)
     ax.set_ylim(0, y_limit)
 
