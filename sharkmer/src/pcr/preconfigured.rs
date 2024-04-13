570efb26427c14ca05f011579610c7be831926a2
Casey Dunn <casey.dunn@yale.edu>
2024-04-13 10:15:19 -0400

    added teleostei primers and datasets

--- sharkmer/src/pcr/preconfigured.rs
+++ sharkmer/src/pcr/preconfigured.rs
@@ -14,6 +14,11 @@ fn get_preconfigured_panels() -> Vec<PCRPanel> {
         description: "18S, 28S, ITS, 16S, and CO1".to_string(),
         params: get_cnidaria(),
     },
+    PCRPanel {
+      name: "teleostei".to_string(),
+      description: "12S and CO1".to_string(),
+      params: get_teleostei(),
+  },
   ];
 
   // Loop over the panels and prepend the PCRPanel name to the PCRParams genename, using _ as a deimiter
@@ -80,6 +85,47 @@ fn get_cnidaria() -> Vec<PCRParams> {
       },	
   ]
 }
+
+fn get_teleostei() -> Vec<PCRParams> {
+  vec![
+    // https://doi.org/10.1098/rstb.2005.1716
+    // expected product is 655bp
+    PCRParams {
+          forward_seq: "TCAACCAACCACAAAGACATTGGCAC".to_string(),
+          reverse_seq: "TAGACTTCTGGGTGGCCAAAGAATCA".to_string(),
+          max_length: 1000,
+          gene_name: "co1".to_string(),
+          coverage: 3,
+          mismatches: 2,
+          trim: 15,
+      },
+    // https://doi.org/10.1371/journal.pone.0266720 evaluates 
+    // multiple primer pairs below
+    
+    // https://doi.org/10.1093/nar/gkr732
+    // expected product is 106bp
+    PCRParams {
+          forward_seq: "ACTGGGATTAGATACCCC".to_string(),
+          reverse_seq: "TAGAACAGGCTCCTCTAG".to_string(),
+          max_length: 200,
+          gene_name: "12s".to_string(),
+          coverage: 3,
+          mismatches: 2,
+          trim: 15,
+      },
+      // https://doi.org/10.1002/ece3.3123
+      // expected product is 219bp
+      PCRParams {
+          forward_seq: "GACCCTATGGAGCTTTAGAC".to_string(),
+          reverse_seq: "CGCTGTTATCCCTADRGTAACT".to_string(),
+          max_length: 300,
+          gene_name: "16s".to_string(),
+          coverage: 3,
+          mismatches: 2,
+          trim: 15,
+      },
+    ]
+  }
 
 pub fn get_panel(panel_name: &str) -> Result<Vec<PCRParams>, String> {
   let panels = get_preconfigured_panels();
