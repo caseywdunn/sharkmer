3724827ad60f2b5df7a8bef848ff165cc1be2aeb
Casey Dunn <casey.dunn@yale.edu>
2024-06-10 12:28:58 -0400

    added genome details and ef1a

--- sharkmer/src/pcr/preconfigured.rs
+++ sharkmer/src/pcr/preconfigured.rs
@@ -45,7 +45,7 @@ fn get_cnidaria() -> Vec<PCRParams> {
           trim: 15,
           citation: "Modified from Cunningham and Buss 1993 https://doi.org/10.1016/0305-1978(93)90009-G ".to_string(),
           notes: "Amplifies portions of domains IV and V. Product is about 580bp".to_string(),
-      },
+    },
 
     PCRParams {
           forward_seq: "TCATAARGATATHGG".to_string(),
@@ -58,7 +58,7 @@ fn get_cnidaria() -> Vec<PCRParams> {
           trim: 15,
           citation: "Modified from Folmer et al. 1994".to_string(),
           notes: "Product is about 700bp long".to_string(),
-      },	
+    },	
 
     PCRParams {
           forward_seq: "AACCTGGTTGATCCTGCCAGT".to_string(),
@@ -71,7 +71,7 @@ fn get_cnidaria() -> Vec<PCRParams> {
           trim: 15,
           citation: "Medlin et al. 1988 https://doi.org/10.1016/0378-1119(88)90066-2".to_string(),
           notes: "Product is about 1790bp".to_string(),
-      },	
+    },	
 
     PCRParams {
           forward_seq: "CCYYAGTAACGGCGAGT".to_string(),
@@ -84,7 +84,7 @@ fn get_cnidaria() -> Vec<PCRParams> {
           trim: 15,
           citation: "Evans et al. 2008. https://doi.org/10.1186/1471-2148-8-139".to_string(),
           notes: "Forward primer is F97, reverse is R3238. Product is about 3230bp.".to_string(),
-      },	
+    },	
 
     PCRParams {
           forward_seq: "TACACACCGCCCGTCGCTACTA".to_string(), // CAS18sF1
@@ -97,9 +97,9 @@ fn get_cnidaria() -> Vec<PCRParams> {
           trim: 15,
           citation: "Ji et al 2003. https://doi.org/10.1046/j.1471-8286.2003.00519.x".to_string(),
           notes: "Forward primer is CAS18sF1, position 1843, from Ji et al 2003. The reverse primer is the reverse complement of the 28s forward primer. Product is about 770-880bp.".to_string(),
-      },	
+    },	
       
-      PCRParams {
+    PCRParams {
         forward_seq: "GTAGGTGAACCTGCAGAAGGATCA".to_string(), // Reverse complement of 18s reverse primer
         reverse_seq: "ACTCGCCGTTACTRRGG".to_string(),
         min_length: 600,
@@ -110,6 +110,19 @@ fn get_cnidaria() -> Vec<PCRParams> {
         trim: 15,
         citation: "Ji et al 2003. https://doi.org/10.1046/j.1471-8286.2003.00519.x".to_string(),
         notes: "Forward primer is reverse complement of 18s reverse primer. The reverse primer is the reverse complement of the 28s forward primer. Product is about 620-730bp.".to_string(),
+    },
+
+    PCRParams {
+        forward_seq: "ACGTGGTATGGTTGCCTCTG".to_string(), // Reverse complement of 18s reverse primer
+        reverse_seq: "CTTGATAACGCCAACGGCWAC".to_string(),
+        min_length: 0,
+        max_length: 3000,
+        gene_name: "ef1a".to_string(),
+        coverage: 3,
+        mismatches: 2,
+        trim: 15,
+        citation: "".to_string(),
+        notes: "".to_string(),
     },	
   ]
 }
