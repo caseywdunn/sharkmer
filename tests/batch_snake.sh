492963758a969f4eafe807e327c39bb902dd048b
Casey Dunn <casey.dunn@yale.edu>
2024-06-11 15:48:56 -0400

    increased ram

--- tests/batch_snake.sh
+++ tests/batch_snake.sh
@@ -6,7 +6,7 @@
 #SBATCH --partition=ycga
 #SBATCH --nodes=1                    # number of cores and nodes
 #SBATCH --cpus-per-task=8           # number of cores
-#SBATCH --mem-per-cpu=12G            # shared memory, scaling with CPU request
+#SBATCH --mem-per-cpu=24G            # shared memory, scaling with CPU request
 
 # For sharkmer, use --partition=ycga_bigmem    --cpus-per-task=16    --mem-per-cpu=60G
 # For most other steups, use --partition=pi_dunn   --cpus-per-task=16   --mem-per-cpu=4G
