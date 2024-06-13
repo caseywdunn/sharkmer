917a5d7146303bd660fad690de11e53d78ee62f5
Casey Dunn <caseywdunn@gmail.com>
2024-06-13 11:30:38 -0400

    remove output that exceeds number of reads sequenced

--- tests/aggregate_stats.py
+++ tests/aggregate_stats.py
@@ -6,6 +6,21 @@ output_directory = './output'
 data_directory = './data'
 benchmark_directory = './benchmarks'
 
+
+
+
+# Get read stats
+fastq_files = [f for f in os.listdir(data_directory) if f.endswith('.fastq')]
+reads_count = {}
+for f in fastq_files:
+    file_path = os.path.join(data_directory, f)
+    with open(file_path, 'r') as file:
+        line_count = sum(1 for line in file)  # Count lines in the file
+        reads_count[f.split('.')[0]] = line_count // 4  # Divide by 4 to get the number of reads
+
+for species, reads in reads_count.items():
+    print(f'{species}: {reads}')
+
 # Get output stats
 # files have naming convention:
 # Xenia_sp_16M_cnidaria_16s.fasta
@@ -30,6 +45,11 @@ output_df = pd.DataFrame(output_stats, columns=columns)
 output_df = output_df.apply(pd.to_numeric, errors='ignore')
 output_df.sort_values(['clade', 'gene', 'million_reads', 'species'], inplace=True)
 
+# For each entry in fastq_files, remove any row from data where the million_reads*1000000 is greater than the number of record in the fastq file
+for species, reads in reads_count.items():
+    output_df = output_df[~((output_df['species'] == species) & (output_df['million_reads']*1000000 > reads))]
+
+
 # save the DataFrame to a CSV file
 output_df.to_csv('output_stats.csv', index=False)
 
@@ -37,26 +57,13 @@ print("Output stats")
 # Print the full DataFrame
 print(output_df)
 
-
-# Get read stats
-fastq_files = [f for f in os.listdir(data_directory) if f.endswith('.fastq')]
-reads_count = {}
-for f in fastq_files:
-    file_path = os.path.join(data_directory, f)
-    with open(file_path, 'r') as file:
-        line_count = sum(1 for line in file)  # Count lines in the file
-        reads_count[f.split('.')[0]] = line_count // 4  # Divide by 4 to get the number of reads
-
-for species, reads in reads_count.items():
-    print(f'{species}: {reads}')
-
 # Ingest benchmarks
 benchmarks = []
 for filename in os.listdir(benchmark_directory):
     if filename.endswith(".benchmark.txt"):
         # Split the filename to extract the required parts
         parts = filename.split('.')
-        sample = parts[1]
+        species = parts[1]
         million_reads = int(parts[2][:-1])  # Remove the 'M' and convert to int
 
         # Read the content of the file
@@ -66,10 +73,10 @@ for filename in os.listdir(benchmark_directory):
             values = lines[1].split()
             
             # Append the benchmarks
-            benchmarks.append([filename, sample, million_reads] + values)
+            benchmarks.append([filename, species, million_reads] + values)
 
 # Define the column names
-columns = ['filename', 'sample', 'million_reads', 's', 'h:m:s', 'max_rss', 'max_vms', 'max_uss', 'max_pss', 'io_in', 'io_out', 'mean_load', 'cpu_time']
+columns = ['filename', 'species', 'million_reads', 's', 'h:m:s', 'max_rss', 'max_vms', 'max_uss', 'max_pss', 'io_in', 'io_out', 'mean_load', 'cpu_time']
 
 # Create a pandas DataFrame
 benchmark_df = pd.DataFrame(benchmarks, columns=columns)
@@ -78,11 +85,11 @@ benchmark_df = pd.DataFrame(benchmarks, columns=columns)
 benchmark_df = benchmark_df.apply(pd.to_numeric, errors='ignore')
 
 # Sort the DataFrame by sample and then by million_reads
-benchmark_df.sort_values(['sample', 'million_reads'], inplace=True)
+benchmark_df.sort_values(['species', 'million_reads'], inplace=True)
 
 # For each entry in fastq_files, remove any row from data where the million_reads*1000000 is greater than the number of record in the fastq file
 for species, reads in reads_count.items():
-    benchmark_df = benchmark_df[~((benchmark_df['sample'] == species) & (benchmark_df['million_reads']*1000000 > reads))]
+    benchmark_df = benchmark_df[~((benchmark_df['species'] == species) & (benchmark_df['million_reads']*1000000 > reads))]
 
 # save the DataFrame to a CSV file
 benchmark_df.to_csv('benchmarks.csv', index=False)
